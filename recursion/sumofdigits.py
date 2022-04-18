2ce2051f109ee3cee464d41367aec985b6949639
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-18 03:38:26 +0100

    Modified sum of digits

--- recursion/sumofdigits.py
+++ recursion/sumofdigits.py
@@ -2,24 +2,36 @@ def sum_ofDigits(n):
     """
     Iterative implementation of sum of digits
     """
-    sum = 0
+    total_sum = 0
     assert n >= 0, "Enter a positive number"
     if n == 0:
         return 0
     else:
         for i in str(n):
-            sum += int(i)
-    return sum
-    
-def sum_of_digits(n):
-    """
-    Recursive implementation of sum of digits
-    """
-    assert n >= 0 and int(n) == n, "Enter a positive value"
-    if n == 0:
+            total_sum += int(i)
+    return total_sum
+
+
+
+digits = input('Enter the digits ')
+def sumdigits(digits):
+    l = len(digits)
+    if l == 0:
         return 0
     else:
-        return int(n%10) + sum_of_digits(int(n/10))
+        return int(digits[0]) + sumdigits(digits[1:])
+    
+print(sumdigits(digits))
+    
+# def sum_of_digits(n):
+#     """
+#     Recursive implementation of sum of digits
+#     """
+#     assert n >= 0 and int(n) == n, "Enter a positive value"
+#     if n == 0:
+#         return 0
+#     else:
+#         return int(n%10) + sum_of_digits(int(n/10))
 
     
 
